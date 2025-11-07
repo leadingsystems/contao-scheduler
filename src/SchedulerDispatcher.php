827dc6dc6b496958647d28d724d6aae4870daa92
Rainer Blechinger <rainer.blechinger@web.de>
2025-11-07 15:55:36 +0100

    use time in settings instead of UTC

--- src/SchedulerDispatcher.php
+++ src/SchedulerDispatcher.php
@@ -144,7 +144,15 @@ class SchedulerDispatcher
     private function check_mustRun(SchedulerJobModel $job): bool
     {
         try {
-            $nextRunDate = CronExpression::factory($job->cronExpression)->getNextRunDate(DateTime::createFromFormat('U', $job->tstampLastRun));
+            $timezoneString = \Contao\Config::get('timeZone'); //if nothing is set in localconfig 'UTC' is the default value
+            $contaoTimezone = new \DateTimeZone($timezoneString);
+
+            $lastRunDate = (new \DateTimeImmutable())
+                ->setTimestamp($job->tstampLastRun)
+                ->setTimezone($contaoTimezone);
+
+            $nextRunDate = \Cron\CronExpression::factory($job->cronExpression)->getNextRunDate($lastRunDate);
+
             return $nextRunDate->getTimestamp() <= time();
         } catch (\Exception $e) {
             return false;
